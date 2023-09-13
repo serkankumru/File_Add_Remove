4e7a9f252710a4cc6628f155d451f3b8b3204d8e
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-13 18:57:11 +0300

    Add files via upload

---
+++ ContentRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+
+namespace DAL
+{
+    public class ContentRepository
+    {
+        public List<Image> List()
+        {
+            var ctx = new NewsEntities();
+            return ctx.Image.ToList();
+        }
+        public bool Delete(int id)
+        {
+            Image ımage = new Image();
+            var ctx =new NewsEntities();
+            ımage = ctx.Image.FirstOrDefault(x => x.Id == id);
+            ctx.Image.Remove(ımage);
+            ctx.SaveChanges();
+            return true;
+        }
+
+        public Image GetId(int id)
+        {
+            Image ımage = new Image();
+            var ctx = new NewsEntities();
+            ımage = ctx.Image.FirstOrDefault(x => x.Id == id);
+            return ımage;
+        }
+        public Image GetName(string name)
+        {
+            Image ımage = new Image();
+            var ctx = new NewsEntities();
+            ımage = ctx.Image.FirstOrDefault(x => x.Name == name);
+            return ımage;
+        }
+
+        public int UploadImageInDataBase(HttpPostedFileBase file, Image image)
+        {
+            image.Image1 = ConvertToBytes(file);
+            var images = new Image
+            {
+                Name = image.Name,
+                FileUrl = image.FileUrl,
+                Image1 = image.Image1
+            };
+            var ctx = new NewsEntities();
+            ctx.Image.Add(images);
+            int i = ctx.SaveChanges();
+            if (i == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            BinaryReader reader = new BinaryReader(image.InputStream);
+            imageBytes = reader.ReadBytes((int)image.ContentLength);
+            return imageBytes;
+        }
+    }
+}
---
+++ WebImageController.cs
@@ -0,0 +1,95 @@
+﻿using DAL;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace News.Controllers
+{
+    public class WebImageController : Controller
+    {
+        // GET: Home  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] files)
+        {
+            ViewBag.files = fileUpload(files);
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase[] files)
+        {
+            ViewBag.files = fileUpload(files);
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult UploadJson(HttpPostedFileBase[] files)
+        {
+            return Json(fileUpload(files).Count>0? "Başarılı.":"Başarısız.");
+        }
+        private List<string> fileUpload(HttpPostedFileBase[] files)
+        {
+            string Name = "";
+            string filePath = "";
+            ContentRepository ctx = new ContentRepository();
+            List<string> names = new List<string>();
+            if (ModelState.IsValid && files != null)
+            {
+                foreach (HttpPostedFileBase file in files)
+                {
+                    if (file != null)
+                    {
+                        Name = Guid.NewGuid().ToString() + "_" + Path.GetFileName(file.FileName);
+                        filePath = Path.Combine(Server.MapPath("~/Images"), Name);
+
+                        names.Add(Name);
+                        ctx.UploadImageInDataBase(file, new DAL.Image() { Name = Name, FileUrl = filePath });
+                        file.SaveAs(filePath);
+                        ViewBag.UploadStatus = files.Count().ToString() + " files uploaded successfully.";
+                    }
+                }
+            }
+            return names;
+        }
+
+        public ActionResult List()
+        {
+            ContentRepository ctx = new ContentRepository();
+            return View(ctx.List());
+        }
+        [HttpPost]
+        public ActionResult List( int id)
+        {
+            ContentRepository ctx = new ContentRepository();
+            DAL.Image ımage = new DAL.Image();
+            if (id != 0)
+            {
+                ımage = ctx.GetId(id);
+                string filePath = Path.Combine(Server.MapPath("~/Images"), ımage.Name);
+
+                if (System.IO.File.Exists(ımage.FileUrl))
+                {
+                    System.IO.File.Delete(ımage.FileUrl);
+                    ctx.Delete(ımage.Id);
+                }
+            }
+            return View(ctx.List());
+        }
+    }
+}
